fd24b5f3163832d467eca35e102b319e0988441e
nithinramkalava <nithinramkalava@gmail.com>
2024-02-23 00:44:46 +0530

    Main file, added grammer examples in resouces

--- Grammar.java
+++ Grammar.java
@@ -41,21 +41,4 @@ public class Grammar {
         }
     }
 
-    public static void clearOutputFile() throws IOException{
-        BufferedWriter clear = new BufferedWriter(new FileWriter("output.txt"));
-        clear.write("");
-        clear.close();
-    }
-
-    public static void main(String[] args) throws IOException{
-        clearOutputFile();
-        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
-
-        Grammar grammer = new Grammar("input.txt", writer);
-        grammer.printGrammer();
-        FirstAndFollow firstAndFollow = new FirstAndFollow(grammer, writer);
-        firstAndFollow.printFirst();
-        firstAndFollow.printFollow();
-        writer.close();
-    }
 }
