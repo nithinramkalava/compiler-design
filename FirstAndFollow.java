025a4be7c29dca7db7a3267694c8c316eace5244
nithinramkalava <nithinramkalava@gmail.com>
2024-02-22 19:05:04 +0530

    can display grammar and compute first

---
+++ FirstAndFollow.java
@@ -0,0 +1,10 @@
+public class FirstAndFollow {
+
+    public static void first(){
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) throws IOException{
+        // BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
+        // String line;
+        // while ((line = bufferedReader.readLine()) != null) {
+        //     System.out.println(Arrays.toString(line.split("->")));
+        // }
+        // bufferedReader.close();
+        System.out.println((int) '\u03B5');
+    }
+}
